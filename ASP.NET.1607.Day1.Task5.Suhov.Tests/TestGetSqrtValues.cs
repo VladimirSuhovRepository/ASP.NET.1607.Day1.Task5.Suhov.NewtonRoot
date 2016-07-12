31d31c1ada7b59511043d1926ec85634e36f5718
vladimir_suhov <vladimir_suhov@outlook.com>
2016-07-12 10:01:00 -0700

    initial

---
+++ ASP.NET.1607.Day1.Task5.Suhov.Tests/TestGetSqrtValues.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace ASP.NET._1607.Day1.Task5.Suhov.Tests
+{
+    [TestClass]
+    public class TestGetSqrtValues
+    {
+        [TestMethod]
+        public void TestIntRootValue()
+        {
+            Assert.AreEqual(SqrtNewton.GetSqrt(32, 5, 0.000001), Math.Pow(32, 1.0 / 5), 0.000001);
+        }
+
+        [TestMethod]
+        public void TestDoubleRootValue()
+        {
+            Assert.AreEqual(SqrtNewton.GetSqrt(32, 7, 0.000001), Math.Pow(32, 1.0 / 7), 0.000001);
+        }
+
+        [TestMethod]
+        public void TestNaNValue()
+        {
+            Assert.AreEqual(SqrtNewton.GetSqrt(double.NaN, 4, 0.1), double.NaN);
+        }
+
+        [TestMethod]
+        public void TestPrecision()
+        {
+            Assert.AreEqual(SqrtNewton.GetSqrt(10, 2, 0.1), Math.Pow(10, 1.0 / 2), 0.1);
+        }
+
+        [TestMethod]
+        public void TestMaxPrecision()
+        {
+            Assert.AreEqual(SqrtNewton.GetSqrt(10, 2, 0.000000000000001), Math.Pow(10, 1.0 / 2), 0.000000000000001);
+        }
+
+        [TestMethod]
+        public void TestNaNRoot()
+        {
+            Assert.AreEqual(SqrtNewton.GetSqrt(-2, 2, 0.1), Math.Pow(-2, 1d / 2));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestArgumentExceptions()
+        {
+            SqrtNewton.GetSqrt(2, 2, 16);
+        }
+
+
+        [TestMethod]
+        public void TestDoubleValue()
+        {
+            Assert.AreEqual(SqrtNewton.GetSqrt(5.6, 2, 0.000001), Math.Pow(5.6, 1.0 / 2), 0.000001);
+        }
+
+        [TestMethod]
+        public void TestZeroValue()
+        {
+            Assert.AreEqual(SqrtNewton.GetSqrt(0, 2, 0.000001), Math.Pow(0, 1.0 / 2), 0.000001);
+        }
+
+    }
+}
---
+++ ASP.NET.1607.Day1.Task5.Suhov/SqrtNewton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP.NET._1607.Day1.Task5.Suhov
+{
+    public static class SqrtNewton
+    {
+        /// <summary>Finding of root</summary>
+        /// <param name="value">Value for which you want to search the root</param>
+        /// <param name="degree">Degree of root</param>
+        /// <param name="precision">Precision of root</param>
+        /// <returns>Root of <paramref name="degree"/>-degree for the <paramref name="value"/></returns>
+        public static double GetSqrt(double value, double degree, double precision)
+        {
+            if (value.Equals(double.NaN)) return double.NaN;
+            if (precision < 1E-16 || precision > 1) throw new ArgumentOutOfRangeException(nameof(precision));
+            if(value<0) return double.NaN;
+            if (value == 0) return 0;
+            if (Math.Abs(value) < precision)  return 0;
+            if (value < 0 && degree % 2 == 0) return double.NaN;
+            double n = degree;
+            double x = 1;
+            double prec = precision;
+            while (1 == 1)
+            {
+                double formula = 1 / n * ((n - 1) * x + (value / Math.Pow(x, n - 1)));
+                if (Math.Abs(x - formula) < prec) return x;
+                x = formula;
+            }
+        }
+    }
+}
